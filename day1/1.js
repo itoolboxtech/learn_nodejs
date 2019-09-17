1b1a4efb11b50b607eb0de1dfd77641df87b7f82
itoolboxtech <34581975+itoolboxtech@users.noreply.github.com>
2019-09-18 05:54:04 +1200

    Update 1.js

--- day1/1.js
+++ day1/1.js
@@ -1,7 +1,8 @@
-var fs = require("fs");  这个是node js内置写法， 引包， 读一个文件
+//这个是node js内置写法， 引包， 读一个文件, file system, 文件系统
+var fs = require("fs");  
 
-fs.readFile(".test.txt", function(err, data) {
-   console.log(data);
+fs.readFile("./test.txt", function(err, data) {
+   console.log(data.toString());
 });
 
 var sum = 0;
