e41ceb7ac9ac632629e1a73747e9a070cc00b060
itoolboxtech <34581975+itoolboxtech@users.noreply.github.com>
2019-09-18 05:55:36 +1200

    Update 1.js

--- day1/1.js
+++ day1/1.js
@@ -1,10 +1,13 @@
 //这个是node js内置写法， 引包， 读一个文件, file system, 文件系统
 var fs = require("fs");  
 
+//读取文件
 fs.readFile("./test.txt", function(err, data) {
+   //回调函数， 读取文件后做的事情
    console.log(data.toString());
 });
 
+//计算
 var sum = 0;
 for(var i = 0; i <= 100; i++) {
   sum += i;
